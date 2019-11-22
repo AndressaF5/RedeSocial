4e8423b100b300cd063a4ac93af20cc95250e370
Carlos Pivotto <carlos.pivotto@prof.infnet.edu.br>
2019-11-22 12:06:24 -0300

    Início do projeto: Commit inicial.

---
+++ Dominio/Arrecadacao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    class Arrecadacao
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Dominio/Doacao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    class Doacao
+    {
+        public int Id { get; set; }
+    }
+}
